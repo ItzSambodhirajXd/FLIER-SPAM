763e7cda1b682f2eb2984d64eff241be2e549934
Atanu9729 <132781152+Atanu9729@users.noreply.github.com>
2023-06-12 15:14:06 +0530

    Add files via upload

---
+++ FLIERBOTS/modules/start.py
@@ -0,0 +1,46 @@
+from telethon import __version__, events, Button
+
+from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
+
+
+START_BUTTON = [
+    [
+        Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
+    ],
+    [
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/FLIERS_SPAMBOT_SUPPORT"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/FLIERS_SPAM_SUPPORT")
+    ],
+    [
+        Button.url("• ʀᴇᴘᴏ •", "https://github.com/Atanu9729/FLIER-SPAM")
+    ]
+]
+
+
+@X1.on(events.NewMessage(pattern="/start"))
+@X2.on(events.NewMessage(pattern="/start"))
+@X3.on(events.NewMessage(pattern="/start"))
+@X4.on(events.NewMessage(pattern="/start"))
+@X5.on(events.NewMessage(pattern="/start"))
+@X6.on(events.NewMessage(pattern="/start"))
+@X7.on(events.NewMessage(pattern="/start"))
+@X7.on(events.NewMessage(pattern="/start"))
+@X8.on(events.NewMessage(pattern="/start"))
+@X9.on(events.NewMessage(pattern="/start"))
+@X10.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+        FLIERBOTS = await event.client.get_me()
+        bot_name = FLIERBOTS.first_name
+        bot_id = FLIERBOTS.id
+        TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝐊ɪʟʟ ✘ 𝐒ᴡɪᴛᴄʜ](https://t.me/Kill_switch16)**\n\n"
+        TEXT += f"» **ғʟɪᴇʀ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
+        TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
+        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
+        await event.client.send_file(
+                    event.chat_id,
+                    "https://telegra.ph/file/b1bc8816c6702c6aa5b71.jpg",
+                    caption=TEXT, 
+                    buttons=START_BUTTON
+                )
