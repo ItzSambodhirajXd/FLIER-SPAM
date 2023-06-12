47bf5c6b35a2577c8b8acc940ae0469602f5a97a
Atanu9729 <132781152+Atanu9729@users.noreply.github.com>
2023-06-12 15:27:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 from telethon import TelegramClient
 
 from os import getenv
-from FLIERBOTS.data import FLIER
+from FLIERBOTS.data import Flier
 
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
